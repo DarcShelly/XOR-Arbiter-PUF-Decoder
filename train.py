39dd75620d49cf782a44206b4ef90fd4f3f69cde
Vaibhav Shaily <vaibhavs21@iitk.ac.in>
2024-12-02 17:19:23 +0530

    code

--- train.py
+++ train.py
@@ -2,10 +2,18 @@ import numpy as np
 import sklearn
 from scipy.linalg import khatri_rao
 
-def my_fit( X_train, y_train ):
-    from sklearn.linear_model import LogisticRegression
+def my_fit( X_train, y_train,  model='LR', penalty='l2', C=6.0, tol=0.0001):
+    if model == 'LR':
+        from sklearn.linear_model import LogisticRegression
+        feat = my_map(X_train)
+        clf = LogisticRegression(dual=False, C=C, penalty=penalty, tol=tol).fit(feat, y_train)
+        w, b = clf.coef_.T.flatten(), clf.intercept_
+        return w, b
+    
+    #else
+    from sklearn.svm import LinearSVC
     feat = my_map(X_train)
-    clf = LogisticRegression(dual=False, C=6.0).fit(feat, y_train)
+    clf = LinearSVC(dual=False, penalty=penalty, C=C, tol=tol).fit(feat, y_train)
     w, b = clf.coef_.T.flatten(), clf.intercept_
     return w, b
 
