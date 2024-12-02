002bc05cc3a3476ce9d0b9e00e836a32f9a28cbd
Vaibhav Shaily <96648106+DarcShelly@users.noreply.github.com>
2024-12-02 16:03:16 +0530

    data and model trainer

---
+++ train.py
@@ -0,0 +1,26 @@
+import numpy as np
+import sklearn
+from scipy.linalg import khatri_rao
+
+def my_fit( X_train, y_train ):
+    from sklearn.linear_model import LogisticRegression
+    feat = my_map(X_train)
+    clf = LogisticRegression(dual=False, C=6.0).fit(feat, y_train)
+    w, b = clf.coef_.T.flatten(), clf.intercept_
+    return w, b
+
+def my_map( X ):
+    X = 2*X-1
+    n=len(X)
+    n_=len(X[0])
+    X = np.flip(np.cumprod(np.flip(X, axis=1), axis=1), axis=1)
+    X = np.append(X, np.ones((X.shape[0], 1)), axis=1)
+    n=len(X)
+    m=len(X[0])
+    feat = np.empty((n, int(m*(m-1)/2)), dtype = X.dtype)
+    ind = 0
+    for i in range(m):
+        for j in range(i+1, m):
+            feat[:, ind] = 2 * X[:, i] * X[:, j]
+            ind+=1
+    return feat
